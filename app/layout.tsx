4b41d9388fe74fd83475dd43e4c1debdcbf7feb8
Naman Gupta <naman.mw4@gmail.com>
2024-07-17 19:52:24 +0530

    Refactor layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,21 +5,23 @@ import Navbar from "./components/Header";
 import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
-
+ 
 export const metadata: Metadata = {
   title: "Nexcent",
   description: "Lessons and Insights from 8 years",
 };
 
 export default function RootLayout({
-	@@ -16,7 +18,11 @@ export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
         <div className="text-neutral-black">{children}</div>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
