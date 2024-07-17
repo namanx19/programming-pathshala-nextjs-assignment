b2b682837218fff3aeee251aced5327e4c10732c
Naman Gupta <naman.mw4@gmail.com>
2024-07-17 19:44:07 +0530

    Fix component name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Header";
 import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
